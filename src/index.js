d842dd5f6b4815fa333d93df07febd00a5462538
James Conkling <james.lane.conkling@gmail.com>
2016-11-04 12:07:56 -0400

    warningfix: don't es6 import and export default in same line

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-export { default as BarChart } from './components/BarChart.jsx';
-export { default as XAxis } from './components/XAxis.jsx';
+// https://github.com/gaearon/react-hot-loader/issues/158
+// export { default as BarChart } from './components/BarChart.jsx';
+// export { default as XAxis } from './components/XAxis.jsx';
+import BarChart from './components/BarChart.jsx';
+import XAxis from './components/XAxis.jsx';
+
+export {
+  BarChart,
+  XAxis
+};
