220c5c46bbf37806de256f93d4d4ef553f95012b
James Conkling <james.lane.conkling@gmail.com>
2016-11-06 16:51:40 -0500

    move charts to different containers

---
+++ example/containers/randomBar.jsx
@@ -0,0 +1,82 @@
+import React              from 'react';
+import * as d3            from 'd3';
+import {
+  BarChart,
+  XAxis,
+  YAxis,
+  Chart
+}                         from '../../src';
+
+const generateData = length => d3.range(1, length + 1).map(idx => ({key: idx, value: Math.random()}));
+
+export default class RandomBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: generateData(20),
+      chartWidth: 600,
+      chartHeight: 300
+    };
+
+    this.randomize = this.randomize.bind(this);
+    this.add = this.add.bind(this);
+    this.remove = this.remove.bind(this);
+    this.shrink = this.shrink.bind(this);
+    this.grow = this.grow.bind(this);
+  }
+
+  randomize() {
+    this.setState({data: generateData(this.state.data.length)});
+  }
+
+  add() {
+    const idx = this.state.data[this.state.data.length -1].key + 1;
+    this.setState({
+      data: [...this.state.data, {key: idx, value: Math.random()}]
+    });
+  }
+
+  remove() {
+    const data = this.state.data;
+    const idx = Math.floor(Math.random() * data.length);
+    this.setState({
+      data: [...data.slice(0,idx), ...data.slice(idx + 1, data.length)]
+    });
+  }
+
+  shrink() {
+    this.setState({chartWidth: this.state.chartWidth - 80});
+  }
+
+  grow() {
+    this.setState({chartWidth: this.state.chartWidth + 80});
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>Chart 1</h3>
+
+        <Chart
+          data={this.state.data}
+          width={this.state.chartWidth}
+          height={this.state.chartHeight}
+        >
+          <BarChart>
+            <XAxis />
+            <YAxis />
+          </BarChart>
+        </Chart>
+
+        <div>
+          <button onClick={this.randomize}>randomize</button>
+          <button onClick={this.add}>+</button>
+          <button onClick={this.remove}>-</button>
+          <button onClick={this.shrink}>shrink</button>
+          <button onClick={this.grow}>grow</button>
+        </div>
+      </section>
+    );
+  }
+}
---
+++ example/containers/streamLine.jsx
@@ -0,0 +1,83 @@
+import React              from 'react';
+import * as d3            from 'd3';
+import Rx                 from 'rxjs';
+import {
+  LineChart,
+  XAxis,
+  YAxis,
+  Chart
+}                         from '../../src';
+
+const makeStream = idx => Rx.Observable
+  .create(observable => {
+    let _idx = idx;
+    const wave = idx => Math.sin(idx) + 1.5;
+
+    setInterval(() => {
+      observable.next([{key: _idx, value: wave(_idx)}]);
+      _idx += 0.1;
+    }, 200);
+  })
+  .map(data => data.map(datum => ({key: +datum.key.toFixed(1), value: +datum.value.toFixed(1)})));
+
+
+export default class StreamLine extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      idx: 1,
+      chartWidth: 600,
+      chartHeight: 300
+    };
+
+    this.$source = null;
+
+    this.toggle= this.toggle.bind(this);
+  }
+
+  subscribe($subject) {
+    return $subject.subscribe(newData => {
+      const { data } = this.state;
+      const joined = [...data, ...newData];
+
+      // tail last x entries in joined data
+      this.setState({
+        data: joined.slice(Math.max(0, joined.length - 40), joined.length),
+        idx: newData[newData.length -1].key
+      });
+    });
+  }
+
+  toggle() {
+    if (this.$source) {
+      this.$source = this.$source.unsubscribe();
+    } else {
+      this.$source = this.subscribe(makeStream(this.state.idx));
+    }
+  }
+
+  render() {
+    return (
+      <section>
+        <h3>Chart 2</h3>
+
+        <Chart
+          data={this.state.data}
+          width={this.state.chartWidth}
+          height={this.state.chartHeight}
+        >
+          <LineChart>
+            <XAxis />
+            <YAxis />
+          </LineChart>
+        </Chart>
+
+        <div>
+          <button onClick={this.toggle}>{this.$source ? 'stop' : 'start'}</button>
+        </div>
+      </section>
+    );
+  }
+
+}
