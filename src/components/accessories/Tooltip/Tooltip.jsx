44ce8c0c17460de19283d377fc5804812c883d78
James Conkling <james.lane.conkling@gmail.com>
2017-01-05 14:27:39 -0500

    add offsets to Tooltip x/y position

--- src/components/accessories/Tooltip/Tooltip.jsx
+++ src/components/accessories/Tooltip/Tooltip.jsx
@@ -3,25 +3,33 @@ import React, {
 } from 'react';
 
 
-const Tooltip = ({ x, y, children }) => (
+const Tooltip = ({ x, y, xOffset, yOffset, children }) => (
   (typeof x === 'number' || typeof x === 'string') &&
   (typeof y === 'number' || typeof y === 'string') ?
     <div
-      style={{ position: 'absolute', top: y, left: x, pointerEvents: 'none' }}
+      style={{
+        position: 'absolute',
+        top: y + yOffset,
+        left: x + xOffset,
+        pointerEvents: 'none'
+      }}
     >
       { children }
     </div>
-  : null
+  : <noscript />
 );
 
 Tooltip.propTypes = {
-  x: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string
-  ]),
-  y: PropTypes.oneOfType([
-    PropTypes.number,
-    PropTypes.string
-  ]),
+  x: PropTypes.number,
+  y: PropTypes.number,
+  xOffset: PropTypes.number,
+  yOffset: PropTypes.number,
   children: PropTypes.node
 };
+
+Tooltip.defaultProps = {
+  xOffset: 0,
+  yOffset: 0
+};
+
+export default Tooltip;
