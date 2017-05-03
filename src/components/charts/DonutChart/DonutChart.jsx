9ba6b6d79fda408affe7d15dcdf765a3fecbf3d8
Jonathan Skeate <skeate@gmail.com>
2017-05-03 14:18:52 -0400

    Wrap label inside donut chart

--- src/components/charts/DonutChart/DonutChart.jsx
+++ src/components/charts/DonutChart/DonutChart.jsx
@@ -1,3 +1,4 @@
+/* eslint-env browser */
 import React, { PropTypes } from 'react';
 import * as d3 from 'd3';
 import {
@@ -11,6 +12,48 @@ import {
   applyEvents2Selection
 } from '../../../utils/events';
 
+function fitsInCircle(radius, width, height) {
+  const angle = Math.atan2(height, width / 2);
+  const x = radius * Math.cos(angle);
+  const y = radius * Math.sin(angle);
+  return x >= width / 2 && y >= height;
+}
+
+function wrapInCircle(text, radius) {
+  text.each(function wrapText() {
+    const textNode = d3.select(this);
+    const words = textNode.text().split(/\s+/);
+    const lineHeight = 25; // px
+    const y = textNode.attr('y');
+    const dy = parseFloat(textNode.attr('dy'));
+    let tspan = textNode.text(null).append('tspan')
+      .attr('x', 0)
+      .attr('y', y)
+      .attr('dy', `${dy}px`);
+    let line = [];
+    let lineNumber = 1;
+    let word = words.pop();
+    let height;
+    while (word) {
+      line.unshift(word);
+      tspan.text(line.join(' '));
+      const width = tspan.node().getComputedTextLength();
+      height = (lineHeight * lineNumber) - dy;
+      if (!fitsInCircle(radius, width, height)) {
+        line.shift();
+        tspan.text(line.join(' '));
+        line = [word];
+        tspan = textNode.append('tspan')
+          .attr('x', 0)
+          .attr('y', y)
+          .attr('dy', `${dy}px`)
+          .text(word);
+        lineNumber += 1;
+      }
+      word = words.pop();
+    }
+  });
+}
 
 class DonutChart extends React.Component {
   componentDidMount() {
@@ -22,7 +65,12 @@ class DonutChart extends React.Component {
   }
 
   update() {
-    let { outerRadius, innerRadius, centerText, centerLabel } = this.props;
+    let { outerRadius, innerRadius } = this.props;
+    const {
+      centerText,
+      centerLabel,
+      labelBuffer
+    } = this.props;
     const { value, data } = this.props;
     const { containerHeight, containerWidth } = this.context;
 
@@ -64,21 +112,17 @@ class DonutChart extends React.Component {
       .classed('yard3-donut-chart-value', true)
       .style('text-anchor', 'middle')
       .attr('dy', +20)
-      .text(function(d) {
-        return centerText;
-      });
+      .text(() => centerText);
 
+    const labelSize = innerRadius - labelBuffer;
     d3.select(this.$chart)
       .append('text')
       .classed('yard3-donut-chart-label', true)
       .style('text-anchor', 'middle')
       .attr('fill', '#aaa')
-      .attr('dy', function(d) {
-        return -20;
-      })
-      .text(function(d) {
-        return centerLabel;
-      });
+      .attr('dy', () => -20)
+      .text(() => centerLabel)
+      .call(wrapInCircle, labelSize);
 
     const slices = $chart.selectAll('.slice');
     applyStyles2Selection(extractStyles(this.props), slices);
@@ -111,13 +155,15 @@ DonutChart.propTypes = {
   innerRadius: PropTypes.number,
   outerRadius: PropTypes.number,
   centerText: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-  centerLabel: PropTypes.string
+  centerLabel: PropTypes.string,
+  labelBuffer: PropTypes.number
 };
 
 DonutChart.defaultProps = {
   value: d => d,
   centerText: null,
-  centerLabel: null
+  centerLabel: null,
+  labelBuffer: 9
 };
 
 DonutChart.contextTypes = {
