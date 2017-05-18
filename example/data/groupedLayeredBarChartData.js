f7f7612dfc6d9a197d64edcd19c4847c881d35e6
jedininjaster <jedi.ninja92@gmail.com>
2017-05-18 19:39:43 -0400

    add grouped layered bar chart

--- example/data/groupedLayeredBarChartData.js
+++ example/data/groupedLayeredBarChartData.js
@@ -6,62 +6,183 @@ module.exports = [
         "barKey": "pizza",
         "groupKey": "March-2017",
         "total": 0,
-        backgroundBar: { "blockKey": "pizza", "groupKey": "March-2017", "barKey": "pizza", "value": 10 },
-        foregroundBar: { "blockKey": "pizza", "groupKey": "March-2017", "barKey": "pizza", "value": 5 }
+        data: [
+          {
+            "blockKey": "pizza",
+            "groupKey": "March-2017",
+            "barKey": "pizza",
+            "value": 10,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "pizza",
+            "groupKey": "March-2017",
+            "barKey": "pizza", "value": 5,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "storage",
         "groupKey": "March-2017",
         "total": 0.051786,
-        backgroundBar: { "blockKey": "storage", "groupKey": "March-2017", "value": 2 },
-        foregroundBar: { "blockKey": "storage", "groupKey": "March-2017", "value": 4 }
+        data: [
+          {
+            "blockKey": "storage",
+            "groupKey": "March-2017",
+            "value": 2,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "storage",
+            "groupKey": "March-2017",
+            "value": 4,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "peppers",
         "groupKey": "March-2017",
         "total": 4.43196,
-        backgroundBar: { "blockKey": "peppers", "groupKey": "March-2017", "value": 4 },
-        foregroundBar: { "blockKey": "peppers", "groupKey": "March-2017", "value": 4 }
+        data: [
+          {
+            "blockKey": "peppers",
+            "groupKey": "March-2017",
+            "value": 4,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "peppers",
+            "groupKey": "March-2017",
+            "value": 4,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "garlicBread",
         "groupKey": "March-2017",
         "total": 0.000007,
-        backgroundBar: { "blockKey": "garlicBread", "groupKey": "March-2017", "value": 7 },
-        foregroundBar: { "blockKey": "garlicBread", "groupKey": "March-2017", "value": 0 }
+        data: [
+          {
+            "blockKey": "garlicBread",
+            "groupKey": "March-2017",
+            "value": 7,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "garlicBread",
+            "groupKey": "March-2017",
+            "value": 0,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "electricity",
         "groupKey": "March-2017",
         "total": 2.993824,
-        backgroundBar: { "blockKey": "electricity", "groupKey": "March-2017", "value": 3.993824 },
-        foregroundBar: { "blockKey": "electricity", "groupKey": "March-2017", "value": 2.993824 }
+        data: [
+          {
+            "blockKey": "electricity",
+            "groupKey": "March-2017",
+            "value": 3.993824,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "electricity",
+            "groupKey": "March-2017",
+            "value": 2.993824,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "rentals",
         "groupKey": "March-2017",
         "total": 10.6,
-        backgroundBar: { "blockKey": "rentals", "groupKey": "March-2017", "value": 10.6 },
-        foregroundBar: { "blockKey": "rentals", "groupKey": "March-2017", "value": 10.6 }
+        data: [
+          {
+            "blockKey": "rentals",
+            "groupKey": "March-2017",
+            "value": 10.6,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "rentals",
+            "groupKey": "March-2017",
+            "value": 10.6,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "labor",
         "groupKey": "March-2017",
         "total": 0.000004,
-        backgroundBar: { "blockKey": "labor", "groupKey": "March-2017", "value": 0.000004 },
-        foregroundBar: { "blockKey": "labor", "groupKey": "March-2017", "value": 0.000004 }
+        data: [
+          {
+            "blockKey": "labor",
+            "groupKey": "March-2017",
+            "value": 0.000004,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "labor",
+            "groupKey": "March-2017",
+            "value": 0.000004,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "bread",
         "groupKey": "March-2017",
         "total": 24.016228,
-        backgroundBar: { "blockKey": "bread", "groupKey": "March-2017", "value": 24.016228 },
-        foregroundBar: { "blockKey": "bread", "groupKey": "March-2017", "value": 15.123411 }
+        data: [
+          {
+            "blockKey": "bread",
+            "groupKey": "March-2017",
+            "value": 24.016228,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "bread",
+            "groupKey": "March-2017",
+            "value": 15.123411,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "delivery",
         "groupKey": "March-2017",
         "total": 0.5,
-        backgroundBar: { "blockKey": "delivery", "groupKey": "March-2017", "value": 0.5 },
-        foregroundBar: { "blockKey": "delivery", "groupKey": "March-2017", "value": 1.5 }
+        data: [
+          {
+            "blockKey": "delivery",
+            "groupKey": "March-2017",
+            "value": 0.5,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "delivery",
+            "groupKey": "March-2017",
+            "value": 1.5,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "idk",
         "groupKey": "March-2017",
         "total": 0.00002,
-        backgroundBar: { "blockKey": "idk", "groupKey": "March-2017", "value": 0.00002 },
-        foregroundBar: { "blockKey": "idk", "groupKey": "March-2017", "value": 0.00002 }
+        data: [
+          {
+            "blockKey": "idk",
+            "groupKey": "March-2017",
+            "value": 0.00002,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "idk",
+            "groupKey": "March-2017",
+            "value": 0.00002,
+            "class": 'background'
+          }
+        ]
       }
     ]
   }, {
@@ -71,62 +192,183 @@ module.exports = [
         "barKey": "pizza",
         "groupKey": "March-2017",
         "total": 0,
-        backgroundBar: { "blockKey": "pizza", "groupKey": "March-2017", "barKey": "pizza", "value": 10 },
-        foregroundBar: { "blockKey": "pizza", "groupKey": "March-2017", "barKey": "pizza", "value": 5 }
+        data: [
+          {
+            "blockKey": "pizza",
+            "groupKey": "March-2017",
+            "barKey": "pizza",
+            "value": 10,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "pizza",
+            "groupKey": "March-2017",
+            "barKey": "pizza", "value": 5,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "storage",
         "groupKey": "March-2017",
         "total": 0.051786,
-        backgroundBar: { "blockKey": "storage", "groupKey": "March-2017", "value": 2 },
-        foregroundBar: { "blockKey": "storage", "groupKey": "March-2017", "value": 4 }
+        data: [
+          {
+            "blockKey": "storage",
+            "groupKey": "March-2017",
+            "value": 2,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "storage",
+            "groupKey": "March-2017",
+            "value": 4,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "peppers",
         "groupKey": "March-2017",
         "total": 4.43196,
-        backgroundBar: { "blockKey": "peppers", "groupKey": "March-2017", "value": 4 },
-        foregroundBar: { "blockKey": "peppers", "groupKey": "March-2017", "value": 4 }
+        data: [
+          {
+            "blockKey": "peppers",
+            "groupKey": "March-2017",
+            "value": 4,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "peppers",
+            "groupKey": "March-2017",
+            "value": 4,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "garlicBread",
         "groupKey": "March-2017",
         "total": 0.000007,
-        backgroundBar: { "blockKey": "garlicBread", "groupKey": "March-2017", "value": 7 },
-        foregroundBar: { "blockKey": "garlicBread", "groupKey": "March-2017", "value": 0 }
+        data: [
+          {
+            "blockKey": "garlicBread",
+            "groupKey": "March-2017",
+            "value": 7,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "garlicBread",
+            "groupKey": "March-2017",
+            "value": 0,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "electricity",
         "groupKey": "March-2017",
         "total": 2.993824,
-        backgroundBar: { "blockKey": "electricity", "groupKey": "March-2017", "value": 3.993824 },
-        foregroundBar: { "blockKey": "electricity", "groupKey": "March-2017", "value": 2.993824 }
+        data: [
+          {
+            "blockKey": "electricity",
+            "groupKey": "March-2017",
+            "value": 3.993824,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "electricity",
+            "groupKey": "March-2017",
+            "value": 2.993824,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "rentals",
         "groupKey": "March-2017",
         "total": 10.6,
-        backgroundBar: { "blockKey": "rentals", "groupKey": "March-2017", "value": 10.6 },
-        foregroundBar: { "blockKey": "rentals", "groupKey": "March-2017", "value": 10.6 }
+        data: [
+          {
+            "blockKey": "rentals",
+            "groupKey": "March-2017",
+            "value": 10.6,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "rentals",
+            "groupKey": "March-2017",
+            "value": 10.6,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "labor",
         "groupKey": "March-2017",
         "total": 0.000004,
-        backgroundBar: { "blockKey": "labor", "groupKey": "March-2017", "value": 0.000004 },
-        foregroundBar: { "blockKey": "labor", "groupKey": "March-2017", "value": 0.000004 }
+        data: [
+          {
+            "blockKey": "labor",
+            "groupKey": "March-2017",
+            "value": 0.000004,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "labor",
+            "groupKey": "March-2017",
+            "value": 0.000004,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "bread",
         "groupKey": "March-2017",
         "total": 24.016228,
-        backgroundBar: { "blockKey": "bread", "groupKey": "March-2017", "value": 24.016228 },
-        foregroundBar: { "blockKey": "bread", "groupKey": "March-2017", "value": 15.123411 }
+        data: [
+          {
+            "blockKey": "bread",
+            "groupKey": "March-2017",
+            "value": 24.016228,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "bread",
+            "groupKey": "March-2017",
+            "value": 15.123411,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "delivery",
         "groupKey": "March-2017",
         "total": 0.5,
-        backgroundBar: { "blockKey": "delivery", "groupKey": "March-2017", "value": 0.5 },
-        foregroundBar: { "blockKey": "delivery", "groupKey": "March-2017", "value": 1.5 }
+        data: [
+          {
+            "blockKey": "delivery",
+            "groupKey": "March-2017",
+            "value": 0.5,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "delivery",
+            "groupKey": "March-2017",
+            "value": 1.5,
+            "class": 'background'
+          }
+        ]
       }, {
         "barKey": "idk",
         "groupKey": "March-2017",
         "total": 0.00002,
-        backgroundBar: { "blockKey": "idk", "groupKey": "March-2017", "value": 0.00002 },
-        foregroundBar: { "blockKey": "idk", "groupKey": "March-2017", "value": 0.00002 }
+        data: [
+          {
+            "blockKey": "idk",
+            "groupKey": "March-2017",
+            "value": 0.00002,
+            "class": 'foreground',
+          },
+          {
+            "blockKey": "idk",
+            "groupKey": "March-2017",
+            "value": 0.00002,
+            "class": 'background'
+          }
+        ]
       }
     ]
   }
